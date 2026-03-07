fb4a3314a601b51e022831aae4d7db2e5efb5c56
YaRo <roman.yatsenko@hneu.net>
2026-03-07 18:23:53 +0200

    Refactor CalculatorApp to encapsulate UI components and improve button handling

--- calculator.py
+++ calculator.py
@@ -30,62 +30,73 @@ class ExtraActionButton(CalcButton):
     color: ft.Colors = ft.Colors.BLACK
 
 
+@ft.control
+class CalculatorApp(ft.Container):
+
+    def init(self):
+        self.width = 290
+        self.bgcolor = ft.Colors.BLACK
+        self.border_radius = ft.BorderRadius.all(20)
+        self.padding = 20
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+
+        self.content = ft.Column(
+            controls=[
+                ft.Row(controls=[self.result], alignment=ft.MainAxisAlignment.END),
+                ft.Row(
+                    controls=[
+                        ExtraActionButton("AC", on_click=self.button_clicked),
+                        ExtraActionButton("+/-"),
+                        ExtraActionButton("%"),
+                        ActionButton("/"),
+                    ],
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("7"),
+                        DigitButton("8"),
+                        DigitButton("9"),
+                        ActionButton("*"),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("4"),
+                        DigitButton("5"),
+                        DigitButton("6"),
+                        ActionButton("-"),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("1", on_click=self.button_clicked),
+                        DigitButton("2"),
+                        DigitButton("3"),
+                        ActionButton("+"),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton("0", expand=2),
+                        DigitButton("."),
+                        ActionButton("="),
+                    ]
+                ),
+            ]
+        )
+
+    def button_clicked(self, event):
+        data = event.control.content
+        if data == "AC":
+            self.result.value = "0"
+        elif data == "1":
+            self.result.value = "1"
+
+
 def main(page: ft.Page):
     page.title = "Калькулятор"
-    result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
-
-    page.add(
-        ft.Container(
-            width=290,
-            bgcolor=ft.Colors.BLACK,
-            border_radius=ft.BorderRadius.all(20),
-            padding=20,
-            content=ft.Column(
-                controls=[
-                    ft.Row(controls=[result], alignment=ft.MainAxisAlignment.END),
-                    ft.Row(
-                        controls=[
-                            ExtraActionButton("AC"),
-                            ExtraActionButton("+/-"),
-                            ExtraActionButton("%"),
-                            ActionButton("/"),
-                        ],
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton("7"),
-                            DigitButton("8"),
-                            DigitButton("9"),
-                            ActionButton("*"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton("4"),
-                            DigitButton("5"),
-                            DigitButton("6"),
-                            ActionButton("-"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton("1"),
-                            DigitButton("2"),
-                            DigitButton("3"),
-                            ActionButton("+"),
-                        ]
-                    ),
-                    ft.Row(
-                        controls=[
-                            DigitButton("0", expand=2),
-                            DigitButton("."),
-                            ActionButton("="),
-                        ]
-                    ),
-                ]
-            ),
-        ),
-    )
+    calc = CalculatorApp()
+    page.add(calc)
 
 
 if __name__ == "__main__":
