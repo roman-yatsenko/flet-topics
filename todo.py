a3c6ff244948cf2604fdab30f24fb28a2ebb9622
YaRo <roman.yatsenko@hneu.net>
2026-03-28 16:07:34 +0200

    Refactor todo list layout and improve task input handling in todo.py

--- todo.py
+++ todo.py
@@ -7,9 +7,25 @@ def main(page: ft.Page):
         new_task.value = ""
         page.update()
 
-    new_task = ft.TextField(hint_text="Що потрібно зробити?")
+    new_task = ft.TextField(hint_text="Що потрібно зробити?", expand=True)
+    tasks_view = ft.Column()
+    container = ft.Column(
+        width=600,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task,
+                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
+                ]
+            ),
+            tasks_view,
+        ],
+    )
 
-    page.add(new_task, ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked))
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(container)
+    page.window.width = 650
+    page.update()
 
 
 ft.run(main)
